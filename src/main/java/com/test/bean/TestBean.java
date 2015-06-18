deb1bb490729ebf16fb9dea2e8afea24e3afc73f
fuse <jurio666@nate.com>
2015-06-18 18:03:09 +0900

    update
    update

---
+++ src/main/java/com/test/bean/TestBean.java
@@ -0,0 +1,15 @@
+package com.test.bean;
+
+/**
+ * Created by Administrator on 2015-06-18.
+ */
+public class TestBean {
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
